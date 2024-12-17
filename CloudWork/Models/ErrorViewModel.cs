192fd30e92e3d09b318e39b2609659bef632782a
flyer-me <wup3306@gmail.com>
2024-12-17 14:58:24 +0800

    添加项目文件。

---
+++ CloudWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CloudWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
