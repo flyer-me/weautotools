17827e38b7d97519d596db7394ab498f58b628d0
flyer-me <wup3306@gmail.com>
2025-01-03 17:19:11 +0800

    更新基架

---
+++ CloudWork.Common/Attributes/ServiceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CloudWork.Common.Attributes
+{
+    /// <summary>
+    /// 用于配置服务生命周期：Service(Scoped(默认), Singleton, Transient)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceAttribute : Attribute
+    {
+        public ServiceAttribute(ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        {
+            Lifetime = lifetime;
+        }
+        public ServiceLifetime Lifetime { get; set; } = ServiceLifetime.Scoped;
+
+    }
+}
