54b5d43f87cc19dbb73f7af15ffab9f0da04bfb6
flyer-me <wup3306@gmail.com>
2025-01-05 16:59:14 +0800

    增加API模式

---
+++ API/App.Core/Program.cs
@@ -0,0 +1,34 @@
+using App.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddIdentityCore<IdentityUser>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("WSLConnection") ?? throw new InvalidOperationException("Connection string 'WSLConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
